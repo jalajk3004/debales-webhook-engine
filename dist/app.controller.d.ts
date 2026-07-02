1be4d9243a9a16a9c2448cfd5d3ea64c8bfab1aa
jalajk3004 <jalajkumar23989@gmail.com>
2026-07-02 23:52:30 +0530

    chore: project scaffold, mongo+redis via docker-compose, health check

---
+++ dist/app.controller.d.ts
@@ -0,0 +1,10 @@
+import { AppService } from './app.service';
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    health(): {
+        status: string;
+        time: string;
+    };
+    getHello(): string;
+}
---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BullModule } from '@nestjs/bullmq';
+import { TenantsModule } from './tenants/tenants.module';
+import { WebhooksModule } from './webhooks/webhooks.module';
+import { EventsModule } from './events/events.module';
+import { RulesModule } from './rules/rules.module';
+import { JobsModule } from './jobs/jobs.module';
+import { ActionsModule } from './actions/actions.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      },
+    }),
+    TenantsModule,
+    WebhooksModule,
+    EventsModule,
+    RulesModule,
+    JobsModule,
+    ActionsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
